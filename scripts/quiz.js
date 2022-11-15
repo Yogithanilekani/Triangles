f0ba04fe95dfe9b83f9c8b4bb6914f81aa78d134
Yogithan10 <yogithanilekani10@gmail.com>
2022-11-15 10:35:57 +0530

    app code

---
+++ scripts/quiz.js
@@ -0,0 +1,33 @@
+const quizform = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-answers-btn");
+const outputEl = document.querySelector(".output");
+
+const correctAnswers = [
+  "180°",
+  "90°",
+  "right angled",
+  "one right angle",
+  "12, 16, 20",
+  "Equilateral triangle",
+  "100°",
+  "30°",
+  "a + b + c",
+  "no",
+  "45°",
+];
+
+function calculateScore() {
+  const formResults = new FormData(quizform);
+  let score = 0,
+    index = 0;
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  // console.log("The score is "+score);
+  outputEl.innerText = "The score is " + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
